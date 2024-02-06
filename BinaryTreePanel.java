8548cef7457a05e87659a027187f6ec7b01cd915
rozhin <118579042+rozhib@users.noreply.github.com>
2024-02-06 08:21:43 -0600

    Update BinaryTreePanel.java

--- BinaryTreePanel.java
+++ BinaryTreePanel.java
@@ -5,32 +5,36 @@ import java.util.LinkedList;
 import java.awt.event.MouseEvent;
 import java.util.Queue;
 
-public class BinaryTreePanel extends JPanel implements MouseListener {
-	private BinaryTree tree;
-	private Queue<BinaryNode> lo;
+public class BinaryTreePanel extends JPanel {
 	
 	public BinaryTreePanel() {
-		tree= GraphicBinaryTreeRunner.tree;
-		addMouseListener(this);
+		System.out.println("hello world");
 	}
 	
 	public void paint(Graphics g) {
-		//1200x480
+		super.paint(g);
+		BinaryTree tree = BinaryTreeFrame.tree;
+		//System.out.println("x");
+		
+		int space = 100;
+		
 		int x = 600;
 		int y = 20;
-		g.setColor(Color.BLUE);
-		int space = 100;
+		
 		drawTree(g, tree.getRoot(), x, y, space);
+		
 	}
 	
 	public void drawTree(Graphics g, BinaryNode n, int x, int y, int space) {
+		//System.out.println(1);
 		if(n!=null) {
 			g.drawString(n.getValue()+"", x, y);
-		}
+		
 		
 		int left= x-space;
 		int right= x+space;
 		int newY = y+90;
+		g.setColor(Color.BLUE);
 		
 		if(n.left()!= null) {
 			g.drawLine(x, y, left,  newY);
@@ -40,14 +44,10 @@ public class BinaryTreePanel extends JPanel implements MouseListener {
 			g.drawLine(x, y, right, newY);
 			drawTree(g, n.right(), right, newY, space/2);
 		}
+	}
 		
 	}
 	
-	public void mousePressed(MouseEvent e) {}
-	public void mouseReleased(MouseEvent e) {}
-	public void mouseEntered(MouseEvent e) {}
-	public void mouseExited(MouseEvent e) {}
-	public void mouseClicked(MouseEvent e) {
-	}
+	
 	
 }
